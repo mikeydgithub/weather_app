144ee14fd860f91d625d0e23db314dcb9d48ce65
mikeydgithub <ampgzo2@gmail.com>
2022-06-13 16:49:56 -0400

    finally tailwind works

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import React, {} from 'react';
+import React from 'react';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
 
 export default function App() {
   return (
     <div className="App">
       <h1 className="text-2xl font-bold text-red-500">Hello world!</h1>
+      <UilReact size={50}/>
     </div>
   )
 }
