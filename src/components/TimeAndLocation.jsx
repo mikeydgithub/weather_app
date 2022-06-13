b3497268b52c64bd5526ff9906f965b6ecc72473
mikeydgithub <ampgzo2@gmail.com>
2022-06-13 17:41:56 -0400

    create time and location

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function TimeAndLocation() {
   return (
-    <div>TimeAndLocation</div>
+    <div>
+        <div className="flex flex-row justify-center my-6">
+            <p className="text-white text-xl font-extralight">
+            Tuesday, 31 May 2022 | Local Time 12:46 PM
+            </p>
+        </div>
+    </div>
+
   )
 }
 
