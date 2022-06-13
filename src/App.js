6b2b4b72385980badc2e3c32b472dd807af4291a
mikeydgithub <ampgzo2@gmail.com>
2022-06-13 17:02:46 -0400

    create component and create topbuttons array and objects

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
 import React from 'react';
-import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons';
+
+
 
 export default function App() {
   return (
-    <div className="App">
-      <h1 className="text-2xl font-bold text-red-500">Hello world!</h1>
-      <UilReact size={50}/>
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br 
+    from bg-cyan-700 to-blue-700 h-fit shadow-xl 
+    shadow-gray-400">
+      <TopButtons/>
     </div>
   )
 }
---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+function TopButtons() {
+
+    // an array of objects
+    const cities = [
+        {
+            id: 1,
+            title: 'London'
+        },
+        {
+            id: 2,
+            title: 'Sydney'
+        },
+        {
+            id: 3,
+            title: 'Tokyo'
+        },
+        {
+            id: 4,
+            title: 'Toronto'
+        },
+        {
+            id: 5,
+            title: 'Paris'
+        }
+    ]
+
+    return (
+        <div>
+        Top Buttons
+        </div>
+    )
+}
+
+export default TopButtons
