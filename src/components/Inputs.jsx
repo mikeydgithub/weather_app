1cb5d8ab4a38222b019782b0369a3cf15e0ad58b
mikeydgithub <ampgzo2@gmail.com>
2022-06-14 10:59:22 -0400

    create function for celcius and farenheit units

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -4,6 +4,10 @@ import { UilSearch, UilLocationPoint  } from '@iconscout/react-unicons'
 function Inputs({setQuery, units, setUnits}) {
     const [city, setCity] = useState('');
 
+    const handleUnitsChange = (e) => {
+        const selectedUnit = e.currentTarget.name
+        if (units !== selectedUnit) setUnits(selectedUnit);
+    }
 
     const handleSearchClick = () => {
         if (city !== '') setQuery({q: city})
@@ -45,14 +49,19 @@ function Inputs({setQuery, units, setUnits}) {
             onClick={handleSearchClick}
             />
             <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"
-            onClick={handleLocationClick}/>
+            onClick={handleLocationClick}
+            />
         </div>
         <div className="flex flex-row w-1/4 items-center justify-center">
-            <button name="metric" className="text-xl text-white font-light transition ease-out hover:scale-125">
+            <button name="metric" className="text-xl text-white font-light transition ease-out hover:scale-125"
+            onClick={handleUnitsChange}
+            >
                 °C
             </button>
             <p className="text-xl text-white mx-1">|</p>
-            <button name="imperial" className="text-xl text-white font-light transition ease-out hover:scale-125">
+            <button name="imperial" className="text-xl text-white font-light transition ease-out hover:scale-125"
+            onClick={handleUnitsChange}
+            >
                 °F 
             </button>
         </div>
