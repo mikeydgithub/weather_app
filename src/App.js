873035455139730df3b174ec560650bd3354fcb1
mikeydgithub <ampgzo2@gmail.com>
2022-06-14 11:23:59 -0400

    done. need to add mobile response

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ export default function App() {
   }
 
   return (
-    <div className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br 
+    <div className={`mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br 
     from-cyan-700 to-blue-700 h-fit shadow-xl 
     shadow-gray-400 ${formatbackground()}`}>
       <TopButtons setQuery={setQuery}/>
