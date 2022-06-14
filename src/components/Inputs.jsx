711e0e278bf3d2746133c4e089cbd401d048bd90
mikeydgithub <ampgzo2@gmail.com>
2022-06-14 10:56:18 -0400

    create function for get weather by location

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -9,6 +9,21 @@ function Inputs({setQuery, units, setUnits}) {
         if (city !== '') setQuery({q: city})
     }
 
+    const handleLocationClick = () => {
+        if (navigator.geolocation) {
+
+            navigator.geolocation.getCurrentPosition((position) => {
+                let lat = position.coords.latitude;
+                let lon = position.coords.longitude;
+
+                setQuery({
+                    lat,
+                    lon
+                });
+            });
+        }
+    };
+
   return (
     
 
@@ -29,7 +44,8 @@ function Inputs({setQuery, units, setUnits}) {
             <UilSearch size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"
             onClick={handleSearchClick}
             />
-            <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"/>
+            <UilLocationPoint size={25} className="text-white cursor-pointer transition ease-out hover:scale-125"
+            onClick={handleLocationClick}/>
         </div>
         <div className="flex flex-row w-1/4 items-center justify-center">
             <button name="metric" className="text-xl text-white font-light transition ease-out hover:scale-125">
